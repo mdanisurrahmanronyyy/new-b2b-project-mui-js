17ec1bb18f1e17685fe4d95b395eb941b611ef68
Md. Anisur Rahman Rony <anisur.rony@technonext.com>
2023-12-13 13:38:31 +0600

    init all previous json data

---
+++ src/json/index.js
@@ -0,0 +1,8 @@
+import airlines from "./airlines.json";
+import FloatingBtn from "./FloatingBtn.json";
+import HeroHeader from "./HeroHeader.json";
+import news from "./news.json";
+import TabList from "./TabList.json";
+import why from "./why.json";
+
+export { airlines, FloatingBtn, HeroHeader, news, TabList, why };
